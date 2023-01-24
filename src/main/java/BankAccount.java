08a07a9e7476f83e2b5071bc0a20e64faba72e9a
W-Sanches94 <117460587+W-Sanches94@users.noreply.github.com>
2023-01-24 15:40:46 +0000

    Properties & Constructors added to BankAccount.Java

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -1,2 +1,23 @@
+import java.time.LocalDate;
+
 public class BankAccount {
+
+    //Properties
+
+    String firstName;
+    String lastName;
+    LocalDate dateOfBirth;
+    int accountNumber;
+    Float balance;
+
+    //Constructor
+
+    public BankAccount (String inputName, String lastName, LocalDate dateOfBirth, int accountNumber, Float balance){
+        this.firstName = inputName;
+        this.lastName = inputName;
+        this.dateOfBirth = dateOfBirth;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
 }
