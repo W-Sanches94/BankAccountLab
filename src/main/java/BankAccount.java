db1706aefad892a81217ce172cb1ec54d6568d75
W-Sanches94 <117460587+W-Sanches94@users.noreply.github.com>
2023-01-24 15:48:38 +0000

    Getters & Setters added to BankAccaount.java

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 
 public class BankAccount {
 
-    //Properties
+    // PROPERTIES
 
     String firstName;
     String lastName;
@@ -10,14 +10,59 @@ public class BankAccount {
     int accountNumber;
     Float balance;
 
-    //Constructor
+    // CONSTRUCTOR
 
-    public BankAccount (String inputName, String lastName, LocalDate dateOfBirth, int accountNumber, Float balance){
-        this.firstName = inputName;
-        this.lastName = inputName;
+    public BankAccount (String firstName, String lastName, LocalDate dateOfBirth, int accountNumber, Float balance){
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.dateOfBirth = dateOfBirth;
         this.accountNumber = accountNumber;
         this.balance = balance;
     }
 
+    // METHODS / BEHAVIOURS
+
+    // GETTERS
+
+    public String getFirstName(){
+        return this.firstName;
+    }
+
+    public String getLastName(){
+        return this.lastName;
+    }
+
+    public LocalDate getDateOfBirth(){
+        return this.dateOfBirth;
+    }
+
+    public int getAccountNumber(){
+        return this.accountNumber;
+    }
+
+    public Float getBalance(){
+        return this.balance;
+    }
+
+    // SETTERS
+
+    public void setFirstName(String name){
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setBalance(Float balance) {
+        this.balance = balance;
+    }
 }
